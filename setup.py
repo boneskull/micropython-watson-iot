cfc48acc738316f740558971c29de21cf9144392
Christopher Hiller <boneskull@boneskull.com>
2017-09-19 12:07:29 -0700

    add setup.py and optimization script
    `optimize_upip.py` pulled from [micropython-lib](https://github.com/micropython/micropython-lib) and modified

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from optimize_upip import OptimizeUpip
+
+setup(
+    name='micropython-ibmiotf',
+    version='0.0.0',
+    packages=find_packages(),
+    description='Unofficial IBM Watson IoT Platform SDK for Devices Running Micropython',
+    url='https://github.com/boneskull/micropython-ibmiotf',
+    author='Christopher Hiller',
+    author_email='boneskull@boneskull.com',
+    maintainer='Christopher Hiller',
+    maintainer_email='boneskull@boneskull.com',
+    license='Apache-2.0',
+    install_requires=(
+        'micropython-umqtt.simple>=1.3.4', 'micropython-umqtt.robust>=1.0',
+        'micropython-logging>=0.1.3'),
+    cmdclass=dict(sdist=OptimizeUpip)
+)
