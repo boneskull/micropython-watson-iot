8f6a6620815dea6dbc369faf2b6ecead0e6cbf08
Christopher Hiller <boneskull@boneskull.com>
2017-09-20 17:34:46 -0700

    update examples

---
+++ example/handle_command.py
@@ -0,0 +1,40 @@
+"""
+This example shows how you can handle a command sent to the device.
+
+Per https://console.bluemix.net/docs/services/IoT/iotplatform_overview.html
+(as of September 20, 2017):
+
+"Commands are the mechanism by which applications communicate with devices. Only
+applications can send commands, and the commands are sent to specific devices.
+The device must determine which action to take on receipt of any given command.
+Devices can be designed to listen for any command or to subscribe to a
+specified list of commands."
+
+Commands can also be sent via RESTful API.
+"""
+
+from uibmiotf import Device
+
+# options can be loaded from uibmiotf.json; see
+# uibmiotf.example.json for example
+my_device = Device(
+    device_id='my-device-id',
+    device_type='my-device-type',
+    org='my-org-id',
+    token='my-device-token'
+)
+
+
+def disconnect():
+    my_device.disconnect()
+    print('disconnected')
+
+
+# create a command handler
+my_device.set_command('disconnect', disconnect)
+# connect to MQTT broker
+my_device.connect()
+
+# blocking wait for command
+while my_device.is_connected:
+    my_device.sync_loop()
---
+++ example/publish_event.py
@@ -0,0 +1,39 @@
+"""
+This example shows how a device can publish an event.
+
+Per https://console.bluemix.net/docs/services/IoT/iotplatform_overview.html
+(as of September 20, 2017):
+
+"Events are the mechanism by which devices publish data to the Watson IoT
+Platform. Devices control the content of their messages, and assign a name for
+each event that is sent. The Watson IoT Platform uses the credentials that are
+attached to each event received to determine which device sent the event. This
+architecture prevents devices from impersonating one another.
+
+Applications can process events in real time, and see the source of the event
+and the data contained in the event. Applications must be configured to define
+which devices and events they subscribe to."
+"""
+
+from uibmiotf import Device
+import utime as time
+
+# options can be loaded from uibmiotf.json; see
+# uibmiotf.example.json for example
+device = Device(
+    device_id='my-device-id',
+    device_type='my-device-type',
+    org='my-org-id',
+    token='my-device-token'
+)
+
+# connect to MQTT broker
+device.connect()
+
+# this will publish an event called "my-event-id" every minute containing JSON
+# object {"alive": true}
+while True:
+    # default is JSON format with QoS 0
+    device.publishEvent('my-event-id', {'alive': True})
+    time.sleep(60)
+
